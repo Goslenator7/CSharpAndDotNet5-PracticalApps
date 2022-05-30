7b932071b7c4fae6378f14fbce0f3b97ce3aca95
ryan <ryancumming7@gmail.com>
2022-05-30 15:10:22 +0100

    Added razor class lib, Employees files

---
+++ NorthwindEmployees/Areas/PacktFeatures/Pages/employees.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;           //IEnumerable<t>
+using System.Linq;                          // ToArray()
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;  //pageModel
+using Packt.Shared;                         // Employee
+
+namespace NorthwindEmployees.MyFeature.Pages
+{
+    public class EmployeesPageModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
