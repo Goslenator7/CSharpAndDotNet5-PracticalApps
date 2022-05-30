0f7f3469de1b96e483a21a1822cbf2141f7ca925
ryan <ryancumming7@gmail.com>
2022-05-30 12:21:25 +0100

    Added model and view files for Suppliers

---
+++ NorthwindWeb/Pages/suppliers.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
+namespace NorthwindWeb.Pages
+{
+    public class SuppliersModel : PageModel
+    {
+        public IEnumerable<string> Suppliers { get; set; }
+
+        public void OnGet()
+        {
+            ViewData["Title"] = "Northwind Website - Suppliers";
+
+            Suppliers = new[]
+            {
+                "Alpha Co.", "Beta Limited", "Gamma Corp"
+            };
+        }
+    }
+}
