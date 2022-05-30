28bb7781015da1d44d237522d6c5d9f759cabd4b
ryan <ryancumming7@gmail.com>
2022-05-30 11:59:33 +0100

    Added Pages folder and razor page plus enable razor pages in startup.cs

--- NorthwindWeb/Startup.cs
+++ NorthwindWeb/Startup.cs
@@ -16,6 +16,7 @@ namespace NorthwindWeb
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,10 +34,14 @@ namespace NorthwindWeb
             app.UseRouting();
 
             app.UseHttpsRedirection();
+            app.UseDefaultFiles(); // index.html, default.html etc.
+            app.UseStaticFiles();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
+                endpoints.MapRazorPages();
+
+                endpoints.MapGet("/hello", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
                 });
